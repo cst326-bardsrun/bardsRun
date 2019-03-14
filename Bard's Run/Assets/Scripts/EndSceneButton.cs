be77c42ec49c746df2a358e263a092fe24b99103
coregalado@csumb.edu <coregalado>
2019-03-14 01:05:28 -0700

    adding basic end scene

---
+++ Bard's Run/Assets/Scripts/EndSceneButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneButton : MonoBehaviour {
+
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
---
+++ Bard's Run/Assets/Scripts/EndSceneScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndSceneScore : MonoBehaviour {
+
+    GameManager gameManager;
+    Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+
+        scoreText.text = "0";
+        //scoreText.text = gameManager.getScore().ToString();
+	}
+}
